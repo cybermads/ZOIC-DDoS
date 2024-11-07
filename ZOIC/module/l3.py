483d53b273c2c908f5958dae80cd613f95edc466
건우Sec <madanokr002@gmail.com>
2024-11-07 20:19:34 +0900

    Update l3.py

--- ZOIC/module/l3.py
+++ ZOIC/module/l3.py
@@ -26,32 +26,40 @@ class TextColors:
     LIGHT_GREEN = "\033[92m" 
     RADICAL_RED = "\033[38;5;160m"
     DARK_GREEN = "\033[38;5;28m"
+    SUPER_BRIGHT_LIME_GREEN = '\033[38;2;102;255;102m'
+    BRIGHT_WHITE = '\033[97m'
+    BRIGHT_LIME_GREEN_RGB = '\033[38;2;50;205;50m'
 
 def logo():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(TextColors.LIGHT_GREEN + """
-        ██╗      █████╗ ██╗   ██╗███████╗██████╗     ██████╗ 
-        ██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗    ╚════██╗
-        ██║     ███████║ ╚████╔╝ █████╗  ██████╔╝     █████╔╝
-        ██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗     ╚═══██╗
-        ███████╗██║  ██║   ██║   ███████╗██║  ██║    ██████╔╝
-        ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    ╚═════╝  
+    print(TextColors.SUPER_BRIGHT_LIME_GREEN + """
+    ██╗      █████╗ ██╗   ██╗███████╗██████╗     ██████╗ 
+    ██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗    ╚════██╗
+    ██║     ███████║ ╚████╔╝ █████╗  ██████╔╝     █████╔╝
+    ██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗     ╚═══██╗
+    ███████╗██║  ██║   ██║   ███████╗██║  ██║    ██████╔╝
+    ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    ╚═════╝  
+    """+ TextColors.RESET)
+    print(TextColors.BRIGHT_LIME_GREEN_RGB + """
+    --------------------------------------
+    Revolt : https://rvlt.gg/PeewQeV9
+    Github : https://github.com/madanokr001
+    coded by 건우Sec
+    --------------------------------------
+        
+    Layer3 commands line:
+        
+    icmp  |  icmpflood
+    ping  |  ping of death
+    exit  |  exit layer3 menu
+
           
-╔════════════════════════════════════════════════════════════════════╗
-║                    https://doxwebd.serveo.net                      ║
-║════════════════════════════════════════════════════════════════════║
-║                                                                    ║
-║                      icmp  |  icmp flood attack                    ║
-║                      ping  |  ping of death                        ║
-║                      exit  |  exit zoic                            ║
-║                                                                    ║                                                                    
-╚════════════════════════════════════════════════════════════════════╝
-""")
+"""+ TextColors.RESET)
 
 def layer3():
     while True:
         logo()
-        select = input(TextColors.GREEN + """═══[root@ZOIC~$]                                                                   
+        select = input(TextColors.SUPER_BRIGHT_LIME_GREEN + """═══[root@ZOIC~$]                                                                   
 ═══> """ + TextColors.RESET)
 
         if select == "icmp" or select.lower() == "1":
