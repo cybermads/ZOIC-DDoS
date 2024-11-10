d1d3a1be3d67269eab94b9a24e6a25e9659ab046
건우Sec <madanokr002@gmail.com>
2024-11-10 18:40:00 +0900

    Update dns.py

--- ZOIC/Tools/dns.py
+++ ZOIC/Tools/dns.py
@@ -20,12 +20,11 @@ class TextColors:
     LIGHT_RED = "\033[91m"
     LIGHT_GREEN = "\033[92m"
     RADICAL_RED = "\033[38;5;160m"
-    SUPER_BRIGHT_LIME_GREEN = '\033[38;2;102;255;102m'
     BRIGHT_LIME_GREEN_RGB = '\033[38;2;50;205;50m'
 
 def dnslockup():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(TextColors.SUPER_BRIGHT_LIME_GREEN + """
+    print(TextColors.BRIGHT_LIME_GREEN_RGB + """
 ██╗      ██████╗  ██████╗██╗  ██╗██╗   ██╗██████╗ 
 ██║     ██╔═══██╗██╔════╝██║ ██╔╝██║   ██║██╔══██╗
 ██║     ██║   ██║██║     █████╔╝ ██║   ██║██████╔╝
@@ -36,7 +35,8 @@ def dnslockup():
     
     s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-    url = input(TextColors.WHITE + "Host > "+ TextColors.RESET)
+    url = input(TextColors.SUPER_BRIGHT_LIME_GREEN + """═══[root@URL]                                                                   
+═══> """ + TextColors.RESET)
 
     ip = socket.gethostbyname(url)
 
