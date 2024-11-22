393c01f89ac967b59168d88e4e743fb24cbf7131
건 우 ☦️ <madanokr002@gmail.com>
2024-11-22 22:25:51 +0900

    Update l7.py

--- ZOIC/module/l7.py
+++ ZOIC/module/l7.py
@@ -159,7 +159,7 @@ def layer7():
                     threads.append(thread)
 
                 while True:
-                    time.sleep(0.01)
+                    time.sleep(0.1)
 
             url = input(TextColors.SUPER_BRIGHT_LIME_GREEN + """
 ═══[root@TARGET-URL]                                                                   
@@ -224,7 +224,7 @@ def layer7():
                     threads.append(thread)
 
                 while True:
-                    time.sleep(0.01)  
+                    time.sleep(0.1)  
 
             url = input(TextColors.SUPER_BRIGHT_LIME_GREEN + """
 ═══[root@TARGET-URL]                                                                   
@@ -284,7 +284,7 @@ def layer7():
                     threads.append(thread)
 
                 while True:
-                    time.sleep(0.01)
+                    time.sleep(0.1)
 
             url = input(TextColors.SUPER_BRIGHT_LIME_GREEN + """
 ═══[root@TARGET-URL]                                                                   
