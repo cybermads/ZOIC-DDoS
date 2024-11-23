0d07f547ba10922357887858ee73f7b76dbee544
건 우 ☦️ <madanokr002@gmail.com>
2024-11-23 17:14:31 +0900

    Update main.py

--- ZOIC/update/main.py
+++ ZOIC/update/main.py
@@ -1,42 +1,21 @@
 import socket
 import os
 import subprocess
-
-class TextColors:
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    PURPLE = '\033[35m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
-    BOLD = "\033[01;01m"
-    DARK_RED = "\033[38;5;124m"
-    CRIMSON = "\033[38;5;196m"
-    TOMATO = "\033[38;5;202m"
-    LIGHT_RED = "\033[91m"
-    LIGHT_GREEN = "\033[92m" 
-    RADICAL_RED = "\033[38;5;160m"
-    DARK_GREEN = "\033[38;5;28m"
-    SUPER_BRIGHT_LIME_GREEN = '\033[38;2;102;255;102m'
+from pystyle import Colorate, Colors
 
 def upadate_main():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(TextColors.SUPER_BRIGHT_LIME_GREEN+ """
+    print(Colorate.Horizontal(Colors.green_to_white, """
 ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗
 ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
 ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  
 ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  
 ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗
  ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝   version 2.3  
-        """+ TextColors.RESET)
+        """))
     
     subprocess.run(['git', 'pull'], check=True)
-    print(TextColors.CYAN + "[*] ZOIC sucessfully updated !!"+ TextColors.RESET)
+    print(Colorate.Horizontal(Colors.cyan_to_green, f"[+] ZOIC Updated !!"))
 
 
     input()
