021a23b283f62e87871a047b33de11e272f5dad1
건 우 ☦️ <madanokr002@gmail.com>
2024-11-23 17:16:21 +0900

    Update rdns.py

--- ZOIC/Tools/rdns.py
+++ ZOIC/Tools/rdns.py
@@ -1,47 +1,28 @@
 import socket
 import os
-
-class TextColors:
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    PURPLE = '\033[35m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
-    BOLD = "\033[01;01m"
-    DARK_RED = "\033[38;5;124m"
-    CRIMSON = "\033[38;5;196m"
-    TOMATO = "\033[38;5;202m"
-    LIGHT_RED = "\033[91m"
-    LIGHT_GREEN = "\033[92m"
-    RADICAL_RED = "\033[38;5;160m" 
-    BRIGHT_LIME_GREEN_RGB = '\033[38;2;50;205;50m'
-    SUPER_BRIGHT_LIME_GREEN = '\033[38;2;102;255;102m'
+from pystyle import Colorate, Colors
 
 def rdns():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(TextColors.SUPER_BRIGHT_LIME_GREEN + """
+    print(Colorate.Horizontal(Colors.green_to_white, """
 ██████╗ ███████╗██╗   ██╗███████╗██████╗ ███████╗███████╗
 ██╔══██╗██╔════╝██║   ██║██╔════╝██╔══██╗██╔════╝██╔════╝
 ██████╔╝█████╗  ██║   ██║█████╗  ██████╔╝███████╗█████╗  
 ██╔══██╗██╔══╝  ╚██╗ ██╔╝██╔══╝  ██╔══██╗╚════██║██╔══╝  
 ██║  ██║███████╗ ╚████╔╝ ███████╗██║  ██║███████║███████╗
 ╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝ 
-        """+ TextColors.RESET)
+        """))
     
     s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-    ip = input(TextColors.SUPER_BRIGHT_LIME_GREEN + """═══[root@IP]                                                                   
-═══> """ + TextColors.RESET)
+    ip = input(Colorate.Horizontal(Colors.green_to_blue,"""
+═══[root@URL]                                                                   
+═══> 
+"""))
 
     domain = socket.gethostbyaddr(ip)
 
-    print(TextColors.CYAN + f"""
+    print(Colorate.Horizontal(Colors.blue_to_cyan, f"""
 
     ════════════════════════════════
             REVERSE DNS INFO
@@ -53,7 +34,7 @@ def rdns():
 
     ════════════════════════════════
         
-        """+ TextColors.RESET)
+        """))
 
 
     input()
