b53e529bf2454f5f3074ecb607b62e9bb7299a9f
건 우 ☦️ <madanokr002@gmail.com>
2024-11-30 14:02:14 +0900

    Update install.py

--- ZOIC/install.py
+++ ZOIC/install.py
@@ -28,11 +28,11 @@ def install():
 ╚█████╔╝╚██████╔╝██║  ██║██║ ╚████║    ██████╔╝╚═╝ ██║╚██████╔╝
  ╚════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═════╝     ╚═╝ ╚═════╝
           """)
-    os.system("pip install aiosonic")
-    os.system("pip install re")
-    os.system("pip install cloudscraper")
-    os.system("pip install aiohttp")
-    os.system("pip install scapy")
+    os.system("pip install aiosonic --break-system-packages")
+    os.system("pip install re --break-system-packages")
+    os.system("pip install cloudscraper --break-system-packages")
+    os.system("pip install aiohttp --break-system-packages")
+    os.system("pip install scapy --break-system-packages")
     os.system("git pull")
 
 if __name__ == "__main__":
