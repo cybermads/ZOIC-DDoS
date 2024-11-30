cf82542cf868269cbaeebf9ceaa74127265d20b2
건 우 ☦️ <madanokr002@gmail.com>
2024-11-30 22:09:22 +0900

    Update l7.py

--- ZOIC/module/l7.py
+++ ZOIC/module/l7.py
@@ -399,7 +399,7 @@ def layer7():
                 }
                 try:
                     async with session.get(url, headers=headers) as response:
-                        print(Colorate.Horizontal(Colors.cyan_to_green, f"[ZOIC] Url : {url} PROXY Request : {proxy} Status : {response.status}"))
+                        print(Colorate.Horizontal(Colors.cyan_to_green, f"[ZOIC] Url : {url} PROXY Request : {get_request} Status : {response.status}"))
                 except aiohttp.ClientConnectorError:
                     print(Colorate.Horizontal(Colors.red_to_white, f"[-] Server has down by ZOIC !!"))
                     if retries > 0:
